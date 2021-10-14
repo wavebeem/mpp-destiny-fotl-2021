75f4245c84e94ebc6a4d9aa902382cc4bd547224
Brian Mock <brian@wavebeem.com>
2021-10-13 22:17:33 -0700

    even nicer

--- src/_data/screenshots.js
+++ src/_data/screenshots.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const fs = require("fs");
 const getImageSize = require("image-size");
-const { AssetCache } = require("@11ty/eleventy-cache-assets");
 
 const folders = ["crucible", "gambit", "vanguard", "cute", "scary"];
 
@@ -27,17 +26,6 @@ function readInfo({ folder, root, filename }) {
     .replace(/[_-]/g, " ");
   const { width, height } = getImageSize(path.join(root, filename));
   return { url, name, width, height };
-}
-
-// Delete `.cache` dir when adding new image files locally
-async function getScreenshotsCached() {
-  const asset = new AssetCache("mpp-destiny-fotl-2021");
-  if (asset.isCacheValid("1d")) {
-    return await asset.getCachedValue();
-  }
-  const data = getScreenshots();
-  await asset.save(data, "json");
-  return data;
 }
 
 module.exports = getScreenshots;
