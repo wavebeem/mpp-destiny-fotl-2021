8e89bc6258376a8855ac9ac7b9b8fb2090573255
Brian Mock <brian@wavebeem.com>
2021-10-13 22:00:52 -0700

    nice

--- src/_data/screenshots.js
+++ src/_data/screenshots.js
@@ -1,27 +1,32 @@
 const path = require("path");
 const fs = require("fs");
-const getImageColors = require("get-image-colors");
 const getImageSize = require("image-size");
 const { AssetCache } = require("@11ty/eleventy-cache-assets");
 
-const basePath = "/static/img/crucible/";
-const root = path.resolve(__dirname, `../${basePath}`);
+const folders = ["crucible", "gambit", "vanguard", "cute", "scary"];
 
 async function getScreenshots() {
-  return await Promise.all(fs.readdirSync(root).map(readInfo));
+  return Object.fromEntries(
+    folders.map((folder) => {
+      return [folder, getOneFolder(folder)];
+    })
+  );
 }
 
-async function readInfo(filename) {
-  const url = basePath + filename;
+function getOneFolder(folder) {
+  const root = path.resolve(__dirname, "../static/img", folder);
+  return fs
+    .readdirSync(root)
+    .map((filename) => readInfo({ folder, root, filename }));
+}
+
+function readInfo({ folder, root, filename }) {
+  const url = `/static/img/${folder}/${filename}`;
   const name = path
     .basename(path.basename(filename, ".png"), ".jpg")
     .replace(/[_-]/g, " ");
-  const file = path.join(root, filename);
-  const { width, height } = getImageSize(file);
-  const colors = await getImageColors(file);
-  const color = colors[0].hex();
-  const obj = { url, name, width, height, color };
-  return obj;
+  const { width, height } = getImageSize(path.join(root, filename));
+  return { url, name, width, height };
 }
 
 // Delete `.cache` dir when adding new image files locally
@@ -30,9 +35,9 @@ async function getScreenshotsCached() {
   if (asset.isCacheValid("1d")) {
     return await asset.getCachedValue();
   }
-  const art = await getScreenshots();
-  await asset.save(art, "json");
-  return art;
+  const data = getScreenshots();
+  await asset.save(data, "json");
+  return data;
 }
 
-module.exports = getScreenshotsCached;
+module.exports = getScreenshots;
